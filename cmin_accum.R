19d1ac3b7acea159a2b5f504c507386c18a96e4a
Emily Ury <ury.emily@gmail.com>
2020-12-23 11:24:39 -0500

    made CO2 accumulation plots

--- cmin_accum.R
+++ cmin_accum.R
@@ -1,7 +1,8 @@
 
 
 library(ggplot2)
-
+library(tidyverse)
+library(lubridate)
 
 #### Cmin accumulation curves
 
@@ -18,8 +19,65 @@ data$pH_treat <- as.factor(data$pH_treat)
 data$Sal_treat <- as.factor(data$Sal_treat)
 
 
-info <- data[,c(1:5)]
-x <- data[,27:35]
+data <- read.csv("cmin_accum_long.csv", head=TRUE)
 
-df <- cbind(info,x)
+S3 <- data[which(data$Site == "3"),]
+
+
+head(S3)
+
+p <- ggplot(S3, aes(Day, flux, linetype = Group))
+p +  stat_smooth() + theme_bw() + facet_grid(alk..treatment~.) +
+        scale_linetype_manual(values=c("solid", "solid", "solid", "dashed", "dashed", "dashed", "dotted", "dotted", "dotted"))
+
+
+
+
+S5 <- data[which(data$Site == "5"),]
+head(S5)
+p <- ggplot(S5, aes(Day, flux, linetype = Group))
+p +  stat_smooth() + theme_bw() + facet_grid(alk..treatment~.) +
+  scale_linetype_manual(values=c("solid", "solid", "solid", "dashed", "dashed", "dashed", "dotted", "dotted", "dotted")) +
+  ylim(0,2150)
+
+
+
+
+
+df <- read.csv("chapter4_mater.csv", head = T)
+head(df)
+
+df$Site <- as.factor(df$Site)
+df$pH_treat <- as.factor(df$pH_treat)
+df$Sal_treat <- as.factor(df$Sal_treat)
+
+site3 <- df[which(df$Site == "3"),]
+site5 <- df[which(df$Site == "5"),]
+
+
+p <- ggplot(df, aes(x=Sal_treat, y=total)) + 
+  geom_boxplot() 
+p + theme_bw() + facet_grid(pH_treat ~ Site)
+
+
+
+#### stats
+
+
+anova <- aov(total ~ Sal_treat*pH_treat, data = site3)
+TukeyHSD((anova))
+
+##multicomp
+#dht <- glht(anova, linfct = mcp(Sal_treat = "Tukey"))
+#summary(dht)
+library(agricolae)
+intx <- with(site3, interaction(pH_treat, Sal_treat))
+mod <- aov(total ~ intx, data = site3)
+out <- HSD.test(mod,"intx", group=FALSE)
+print(out$comparison)
+out <- HSD.test(mod,"intx", group=TRUE)
+out
+
+
+
 
