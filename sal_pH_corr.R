c303b2b6b318506f09ed8ae949e3b28d2df08a5e
Emily Ury <ury.emily@gmail.com>
2021-01-08 10:29:43 -0500

    new final figures

--- sal_pH_corr.R
+++ sal_pH_corr.R
@@ -15,31 +15,25 @@ data$fSite <- as.factor(data$Site)
 data$pH_treat <- as.factor(data$pH_treat)
 
 
-fit <- lm(pH_init ~ pH_treat + fSite, data = data)
-summary(fit)
-fit <- lm(pH_init ~ Sal_treat + fSite, data = data)
-summary(fit)
-
-fit <- lm(pH_init ~ pH_treat + Sal_treat + fSite, data = data)
-summary(fit)
-
-
 
 labs <- c("Ponzer muck", "Hyde loam")
 names(labs) <- c("3", "5")
-ggplot(data=data, aes(x=Sal_treat, y = pH_init, color = pH_treat)) + 
-  stat_summary(fun=mean, geom="line", aes(group = pH_treat)) +
-  geom_point(data = data, aes(x = sal_init, y = pH_init, color = pH_treat)) +
+p1 <- ggplot(data=data, aes(x=Sal_treat, y = pH_init)) + 
+  stat_summary(fun=mean, geom="line", aes(group = pH_treat, color = pH_treat)) +
+  geom_point(data = data, aes(x = sal_init, y = pH_init, color = pH_treat, shape = pH_treat)) +
   #geom_smooth(method = "lm") +
   facet_grid(. ~ fSite, labeller = labeller(fSite = labs)) +
   theme_bw() +
   xlab("Salinity (ppt)") +
   ylab("pH") + 
-  labs(color = "pH")
+  scale_shape_manual(values = c(0,16, 3)) +
+  theme(legend.title = element_blank(), legend.position = c(.25,.68), legend.direction = "vertical", 
+        legend.background = element_blank()) 
+
 
 
 ### same figure as above but black and white
-ggplot(data=data, aes(x=Sal_treat, y = pH_init, shape = pH_treat)) + 
+ggplot(data=data, aes(x=Sal_treat, y = pH_init)) + 
   stat_summary(fun=mean, geom="line", aes(group = pH_treat, linetype = pH_treat)) +
   geom_point(data = data, aes(x = sal_init, y = pH_init, shape = pH_treat)) +
   #geom_smooth(method = "lm") +
@@ -47,7 +41,61 @@ ggplot(data=data, aes(x=Sal_treat, y = pH_init, shape = pH_treat)) +
   theme_bw() +
   xlab("Salinity (ppt)") +
   ylab("pH") + 
-  labs(shape = "pH") +
-  scale_shape_manual(values = c(0,16, 3)) 
+  labs(shape = "pH:") + 
+  labs(linetype = " ")+
+  scale_shape_manual(values = c(0,16, 3)) +
+  theme(legend.position = "top")
 # scale_linetype_manual(values = c("solid", "longdash","dotted"))
+
+
+
+
+#### final extract
+data$fSal_treat <- as.factor(data$Sal_treat)
+
+
+p2 <- ggplot(data=data, aes(x=fSal_treat, y = pH_end)) + 
+  #stat_summary(fun=mean, geom="line", aes(group = pH_treat, linetype = pH_treat)) +
+  stat_smooth(data=data, method = "lm", se = FALSE, size = 0.5, aes(x=sal_end, y = pH_end, group = pH_treat, color = pH_treat)) +
+  geom_point(data = data, aes(x = sal_end, y = pH_end, color = pH_treat, shape = pH_treat)) +
+  #geom_smooth(method = "lm") +
+  facet_grid(. ~ fSite, labeller = labeller(fSite = labs)) +
+  theme_bw() +
+  xlab("Salinity (ppt)") +
+  ylab("pH") + 
+  scale_shape_manual(values = c(0,16, 3)) +
+  theme(legend.position = "none")
+ 
+
+
+grid.arrange(p1, p2, nrow = 2)
+
+
+
+
+
+
+
+p3 <- ggplot(data=data, aes(x=fSal_treat, y = sal_end)) + 
+  geom_boxplot(data = data, aes(group = fSal_treat)) + 
+  facet_grid(. ~ fSite, labeller = labeller(fSite = labs)) + 
+  theme_bw() +
+  xlab("Salinity Treatment") +
+  ylab("Final Salinity (ppt)") + 
+  theme(legend.position = "none")
+
+
+p4 <- ggplot(data=data, aes(x=pH_treat, y = pH_end)) + 
+  geom_boxplot(data = data, aes(group = pH_treat)) + 
+  facet_grid(. ~ fSite, labeller = labeller(fSite = labs)) + 
+  theme_bw() +
+  xlab("Salinity Treatment") +
+  ylab("Final pH") + 
+  theme(legend.position = "none")
+
+
+grid.arrange(p1, p3, p4, nrow = 3)
+
+
+
 
