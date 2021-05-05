12f045783fe59feabcc37643ac6c07a9ad0105b1
Emily Ury <ury.emily@gmail.com>
2021-05-05 12:40:36 -0400

    cleaned code

--- normality ch.R
+++ normality ch.R
@@ -1,7 +1,7 @@
 
 
 ## normality check
-
+library(rcompanion)
 
 setwd("C:/Users/uryem/Dropbox (Duke Bio_Ea)/My data/chapter 4")
 
@@ -40,14 +40,11 @@ data$response <- data$SUVA254_end ## not normal
 ### model
 ### TWO-WAY ANOVA
 res.aov <- aov(response ~ Sal_treat*fSite, data = data)
-
-
 # Extract the residuals
 aov_residuals <- residuals(object = res.aov)
 # Run Shapiro-Wilk test
-shapiro.test(x = aov_residuals )
-
-
+shapiro.test(x = aov_residuals )  ## p < 0.05 indicates the residuals are not normal, (the data is non parametric)
+                                  ## and the kruskal-wallis test should be used instead of ANOVA
 
 
 
@@ -56,41 +53,18 @@ shapiro.test(x = aov_residuals )
 Site3 <- data[which(data$Site == "3"),]
 Site5 <- data[which(data$Site == "5"),]
 
-Site3$response <- Site3$C_end ## NORMAL
-Site3$response <- Site3$DOC_mg_L_end ##  NORMAL
-Site3$response <- Site3$Phenol_mg_L_end ## NORMAL
-Site3$response <- Site3$SUVA254_end ## NORMAL
-Site3$response <- Site3$sal_init ## not normal
-Site3$response <- Site3$sal_end ## not normal
-Site3$response <- Site3$ugC.CO2_hr_gc ## not normal
-Site3$response <- Site3$ugC.CO2_hr_gc_pr ## not normal
+kt3 <- kruskal.test(response ~ Sal_treat, data = Site3)
+kt3
+pairwise.wilcox.test(Site3$response, Site3$Sal_treat, p.adjust = "bonferroni")
 
-kt <- kruskal.test(response ~ Sal_treat, data = Site3)
-kt
-pairwise.wilcox.test(Site3$response, Site3$Sal_treat)
-
-Site5$response <- Site5$C_end ## NORMAL
-Site5$response <- Site5$DOC_mg_L_end ##  NORMAL
-Site5$response <- Site5$Phenol_mg_L_end ## NORMAL
-Site5$response <- Site5$SUVA254_end ## not Normal
-Site5$response <- Site5$sal_init ## not normal
-Site5$response <- Site5$sal_end ## not normal
-Site5$response <- Site5$ugC.CO2_hr_gc ## not normal
-Site5$response <- Site5$ugC.CO2_hr_gc_pr ## not normal
-
-
-kruskal.test(response ~ Sal_treat, data = Site5)
+kt5 <- kruskal.test(response ~ Sal_treat, data = Site5)
+kt5 
 pairwise.wilcox.test(Site5$response, Site5$Sal_treat)
-
-
-
 
 
 ### for the non-parametric responses with two groups (Site and Salt Treatment)
 ### we use the ScheirerRayHare extension of Kruskal wallis test in the package 'rcompanion'
-
-
-library(rcompanion)
+#library(rcompanion)
 
 scheirerRayHare(response ~ Sal_treat*fSite, data = data)
 
